d99cc1edbda7975c7a274c9efb90a8040c02eda8
sumit kushwah <sk17595221@gmail.com>
2022-04-16 23:55:40 +0530

    Add files via upload

---
+++ ravi.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class ravi {  
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Hi This is a test of YOu are  in Depression or not ");
+	System.out.println("Feelings of sadness, tearfulness, emptiness or hopelessness");
+	int a= sc.nextInt();  
+    int marks=65;  
+      
+    if(a<=1){  
+        System.out.println("you are depressed");  
+    }  
+    else{  
+        System.out.println("good");  
+    }  
+}  
+}  
